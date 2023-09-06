316c51829c8caf1e9f6ef12c2246bde25511d19f
Glitch (scheduledlinks) <none>
2023-09-06 15:15:58 +0000

    🖌️🌾 Updated with Glitch

--- redirect.js
+++ redirect.js
@@ -2,9 +2,11 @@ if (location.href.substring(0, 5) == "http:") {
   location.href = "https://" + location.href.substring(7, location.href.Length);
 }
 
-var cookieList = document.cookie.split(';');
+var cookieList = document.cookie.split(";");
 
-var records = window.localStorage.records ? JSON.parse(window.localStorage.records) : [];
+var records = window.localStorage.records
+  ? JSON.parse(window.localStorage.records)
+  : [];
 
 var dt = new Date();
 
@@ -12,7 +14,10 @@ function isInTimeLimit(startTime, endTime, currentTime) {
   if (currentTime.hour < startTime.hour || currentTime.hour > endTime.hour) {
     return false;
   }
-  if (startTime.minute > currentTime.minute && currentTime.hour == startTime.hour) {
+  if (
+    startTime.minute > currentTime.minute &&
+    currentTime.hour == startTime.hour
+  ) {
     return false;
   }
   if (endTime.minute < currentTime.minute && currentTime.hour == endTime.hour) {
@@ -24,25 +29,31 @@ function isInTimeLimit(startTime, endTime, currentTime) {
 var linksToOpen = [];
 
 for (var i = 0; i < records.length; i++) {
-  var startTime = records[i][1]
-  var startTimeObj = { hour: startTime.split(":")[0], minute: startTime.split(":")[1] };
-  var endTime = records[i][2]
-  var endTimeObj = { hour: endTime.split(":")[0], minute: endTime.split(":")[1] };
+  var startTime = records[i][1];
+  var startTimeObj = {
+    hour: startTime.split(":")[0],
+    minute: startTime.split(":")[1],
+  };
+  var endTime = records[i][2];
+  var endTimeObj = {
+    hour: endTime.split(":")[0],
+    minute: endTime.split(":")[1],
+  };
   var currentTimeObj = { hour: dt.getHours(), minute: dt.getMinutes() };
   if (isInTimeLimit(startTimeObj, endTimeObj, currentTimeObj)) {
     linksToOpen.push(records[i][0]);
   }
 }
 
+linksToOpen = linksToOpen.reverse();
+
 if (linksToOpen.length == 1) {
   document.location.href = linksToOpen[0];
-}
-else {
+} else {
   for (var i = 0; i < linksToOpen.length; i++) {
     if (i == linksToOpen.length - 1) {
       window.open(linksToOpen[i], "_self");
-    }
-    else {
+    } else {
       window.open(linksToOpen[i]);
     }
   }
